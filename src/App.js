af6ea0ac8e5d8151cdd3ee6dd4a8d61827524cf4
Pratik Jagdale <pratikjagdale111@gmail.com>
2022-04-15 19:43:44 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header/Header"
+import ChatBox from './Components/ChatBox/ChatBox';
+import SongsList from './Components/SongsList/SongsList';
+import {Provider} from "react-redux"
+import store from "./store"
 
 function App() {
+  
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App_Layout'>
+          <div className='App__Top'>
+          <Header/>
+          </div>
+          <div className='App_Bottom'>
+            <SongsList/>
+            <ChatBox/>
+            <SongsList/>
+          </div>
+      </div>
     </div>
+    </Provider>
   );
 }
 
